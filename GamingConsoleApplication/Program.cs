3e961e1975b993846f8453aadaa563962e41a403
TakacsLaszlo01 <takacs.laszlo.istvan.01.moricz@gmail.com>
2026-05-27 12:43:39 +0200

    Konzolos Insert és Select

--- GamingConsoleApplication/Program.cs
+++ GamingConsoleApplication/Program.cs
@@ -17,12 +17,31 @@ internal class Program
             input = Utils.Input("Válasszon műveletet: ").ToUpper();
             switch(input)
             {
-                case "D":
+                case "I": InsertGame();
                     break;
                 default: break;
             }
         }
         while (!input.Equals("X"));
     }
+    private static void InsertGame()
+    {
+        int id, ageLimit;
+        string title;
+        DateTime release;
 
+        while (!int.TryParse(Utils.Input("Adjon meg egy sorszámot:"), out id))
+            Console.WriteLine("Pozitív egészet adjon meg!");
+        title = Utils.Input("Adja meg a játék címét: ");
+
+        int year, month, day;
+        while(!int.TryParse(Utils.Input("Adja meg a megjelenés évét:"), out year) && year <= 1970)
+            Console.WriteLine("1970 utáni évszámot adjon meg!");
+
+        while (!int.TryParse(Utils.Input("Adja meg a hónapot:"), out month) && (month < 1 || month > 12))
+            Console.WriteLine("1 és 12 közötti számot adjon meg!");
+
+        while (!int.TryParse(Utils.Input("Adja meg a napot:"), out month) && (month < 1 || month > 12))
+            Console.WriteLine("1 és 31 közötti számot adjon meg!");
+    }
 }
